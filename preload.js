3b638718f790dcb032dfb0975864652de661282e
Vasant19 <vasantdave2002@gmail.com>
2024-11-04 18:41:23 -0500

    reverted to starter code, v1.0

--- preload.js
+++ preload.js
@@ -1,6 +1,10 @@
-const { contextBridge, ipcRenderer } = require('electron/renderer')
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
 
-contextBridge.exposeInMainWorld('darkMode', {
-  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
-  system: () => ipcRenderer.invoke('dark-mode:system')
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
 })
