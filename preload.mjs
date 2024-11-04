f2e8fa1465bffb8f66ac5d3cda8304bfe40220ab
Vasant19 <vasantdave2002@gmail.com>
2024-11-04 11:50:35 -0500

    Simple UI with added Dark mode toggle, v1.01

---
+++ preload.mjs
@@ -0,0 +1,17 @@
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('versions', {
+    node: () => process.versions.node,
+    chrome: () => process.versions.chrome,
+    electron: () => process.versions.electron,
+    ping: () => ipcRenderer.invoke('ping')
+});
+
+contextBridge.exposeInMainWorld('darkMode', {
+    toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+    system: () => ipcRenderer.invoke('dark-mode:system')
+});
+
+contextBridge.exposeInMainWorld('taskManager', {
+    addTask: (taskText) => ipcRenderer.invoke('add-task', taskText)
+});
